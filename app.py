7fb78c0f719ff2f81116bf6ee1c286568a86deb9
alex <kooptsov@gmail.com>
2021-01-20 15:17:12 +0300

    добавлены файлы и потоки с камер. выбор из списка.

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from dl.detec_tracking_db import  multiDetect
 from werkzeug.utils import secure_filename
 from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+from wtforms.validators import InputRequired
 
 UPLOAD_FOLDER = './static/uploads'
 
@@ -47,7 +50,15 @@ class Video(db.Model):
         self.comment = comment
         self.path = path
 
+
+class ChoiceSourc(FlaskForm):
+    group_web = SelectField('Web')
+    group_file = SelectField('File')
+
+
 db.create_all()
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -56,34 +67,39 @@ def hello_world():
             "home.html"
         )
     else:
-        form_source = request.form["source"]
-        video_flow = request.form["video"]
-        print(video_flow)
-        filename = ''
-        # form_file = request.form["file"]
-        if 'file' not in request.files and  video_flow== '':
-            print('No file part')
-            return redirect(request.url)
-        if video_flow == 'OffLine':
-            f = request.files["file"]
-            filename = f.filename
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        messages = json.dumps({"form_source": form_source, 'filename':filename, 'video_flow':video_flow})
-        session['messages'] = messages
-        print('2')
-        return redirect(url_for("start", messages=messages))
+        if'Cameras' in  request.form:
+            return redirect(url_for("cameras"))
+        if 'Files' in  request.form:
+            return redirect(url_for("files"))
+        if 'Detection' in  request.form:
+            return redirect(url_for("start"))
+
 
 @app.route('/start', methods=['GET', 'POST'])
 def start():
     if request.method == "GET":
-        messages = request.args['messages']  # counterpart for url_for()
-        # messages = session['messages']
-        messages = json.loads(messages)
+        groups_web_list = [(i.path, i.path) for i in Camera.query.all()]
+        groups_web_file = [(i.path, i.path) for i in Video.query.all()]
+        form = ChoiceSourc()
+        form.group_web.choices = groups_web_list
+        form.group_file.choices = groups_web_file
         return render_template(
-            "start.html", source=messages['form_source'], file=messages['filename'], flow=messages['video_flow']
+            "start.html", form=form
         )
     else:
-        return redirect(url_for("detector"))
+        form = ChoiceSourc()
+
+        if'Web' in  request.form:
+            sourc = form.group_web.data.strip()
+            messages = json.dumps({"form_source": 'OnLine', "filename": sourc})
+
+        if 'File' in  request.form:
+            sourc = form.group_file.data.strip()
+            messages = json.dumps({"form_source": 'OffLine', "filename": sourc})
+
+        session['messages'] = messages
+        return redirect(url_for("detector", sourc = sourc))
+
 
 @app.route('/detector', methods=['GET', 'POST'])
 def detector():
@@ -92,14 +108,80 @@ def detector():
         messages = json.loads(messages)
 
         filename = messages['filename']
-        flow = messages['video_flow']
         type = messages['form_source']
         # print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         if filename != '' and type == 'OffLine':
             multiDetect(os.path.join(app.config['UPLOAD_FOLDER'], filename), 1, '1')
-        if flow != '' and type == 'OnLine':
-            multiDetect(flow, 1, '1')
+        if filename != '' and type == 'OnLine':
+            multiDetect(filename, 1, '1')
 
         return render_template(
             "detector.html"
         )
+
+
+@app.route('/cameras', methods=['GET', 'POST'])
+def cameras():
+    if request.method == "GET":
+        return render_template(
+            "cameras.html"
+        )
+    else:
+        if'All' in  request.form:
+            return render_template('cameras_all.html', list=Camera.query.all())
+        if 'Add' in  request.form:
+            return redirect(url_for("new_cam"))
+
+
+@app.route('/new_cam', methods=['GET', 'POST'])
+def new_cam():
+    if request.method == 'POST':
+        if not request.form['name'] or not request.form['room'] or not request.form['path']:
+            flash('Please enter all the fields', 'error')
+        else:
+            cam = Camera(request.form['name'], request.form['room'],
+                               request.form['path'])
+
+            db.session.add(cam)
+            db.session.commit()
+            flash('Record was successfully added')
+            return redirect(url_for('cameras_all'))
+    return render_template('cameras_new.html')
+
+
+@app.route('/cameras_all')
+def cameras_all():
+   return render_template('cameras_all.html', list = Camera.query.all() )
+
+
+@app.route('/files', methods=['GET', 'POST'])
+def files():
+    if request.method == "GET":
+        return render_template(
+            "file.html"
+        )
+    else:
+        if'All' in  request.form:
+            return render_template('files_all.html', list=Video.query.all())
+        if 'Add' in  request.form:
+            return redirect(url_for("file_add"))
+
+
+@app.route('/file_add', methods=['GET', 'POST'])
+def file_add():
+    if request.method == 'POST':
+        if not request.form['comment'] or not request.form['path']:
+            flash('Please enter all the fields', 'error')
+        else:
+            cam = Video(request.form['comment'],    request.form['path'])
+
+            db.session.add(cam)
+            db.session.commit()
+            flash('Record was successfully added')
+            return redirect(url_for('files_all'))
+    return render_template('files_new.html')
+
+
+@app.route('/files_all')
+def files_all():
+   return render_template('files_all.html', list = Video.query.all() )
